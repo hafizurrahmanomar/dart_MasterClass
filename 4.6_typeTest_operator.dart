db7587f949424747cf20a800fac5f341fa523ac8
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-11-04 21:58:08 +0600

    ignore: unnecessary_type_check

--- 4.6_typeTest_operator.dart
+++ 4.6_typeTest_operator.dart
@@ -2,6 +2,8 @@ void main() {
   String value1 = "Dart Bangla Tutorial";
   int age = 20;
   
+  // ignore: unnecessary_type_check
   print(value1 is String);
+  // ignore: unnecessary_type_check
   print(age is !int);
 }
