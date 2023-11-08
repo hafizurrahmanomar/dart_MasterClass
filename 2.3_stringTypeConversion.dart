8f51fe35f4393a59e7a11cbdba00828744cebbc4
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-11-08 18:14:17 +0600

    String Update

---
+++ 2.3_stringTypeConversion.dart
@@ -0,0 +1,28 @@
+void main() {
+  int age = 15;
+  double num = 4.80;
+  String name = 'Nayeem';
+  String doubleValue = '3.12';
+  String stringValue = '3';
+
+  //int convert to String
+  String newString = age.toString();
+  print(newString);
+  print(newString.runtimeType);
+
+  // double convert to int
+  int intNum = num.toInt();
+  print(intNum);
+  // int convert to double
+  double doubleNum = age.toDouble();
+  print(doubleNum);
+
+  // Number String convert to int and double
+  int newInt = int.parse(stringValue);
+  print(newInt);
+  print(newInt.runtimeType);
+
+  double convertDouble = double.parse(doubleValue);
+  print(convertDouble);
+  print(convertDouble.runtimeType);
+}
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,18 @@
+String userDetailsSecondary =
+  //     'User name is' + userName + ' and ' + age.toString() + ' is age';
+  // userDetailsSecondary =
+  //     'User name is $userName and Age is $age'; // String interpolation
+  // print(userDetailsSecondary);
+  // print(userDetailsSecondary.length);
+
+  // print(userDetails.length);
+  // print(userDetails.contains('25'));
+  // print(userDetails.endsWith('25'));
+  // print(userDetails.startsWith('25'));
+  // print(userDetails.toLowerCase());
+  // print(userDetails.toUpperCase());
+  // print(userDetails[0]);
+  // print(userDetails[1]);
+  // double pi = 3.141638748374;
+  // print(pi.toStringAsFixed(2));
+  // print(pi);
