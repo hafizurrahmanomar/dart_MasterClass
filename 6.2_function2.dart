e6b9a4b59fd19d0d6b63d3e4a24f6ed9aa57ffa2
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-11-11 17:47:33 +0600

    update

--- 6.2_function2.dart
+++ 6.2_function2.dart
@@ -10,7 +10,7 @@ void main() {
 }
 
 //Function start
-
+//flutter this type of function is most used
 void userInfo(
     {required String name, required int age, String dist = 'Unknown'}) {
   print("User name: $name\n User age: $age\n Home Town: $dist\n");
