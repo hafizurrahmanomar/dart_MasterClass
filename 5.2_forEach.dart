b30c2fe54f2aad47efbaf0dbeb63ebc0f3f6b7ef
Hafizur Rahman Omar <hafizurrahmanomar@gmail.com>
2023-11-13 17:58:38 +0600

    Type Of Function Update

--- 5.2_forEach.dart
+++ 5.2_forEach.dart
@@ -6,6 +6,19 @@ void main() {
   });
 print("\nFor in loop\n");
 
+//Print Each Total and Average Of Lists
+List<int> numbers = [1,2,3,4,5];
+  
+  int total = 0;
+  
+  numbers.forEach( (num)=>total= total+ num);
+  
+  print("Total is $total.");
+  
+  double avg = total / (numbers.length);
+  
+  print("Average is $avg.");
+
   // standard way
   for (String value in cityName) {
     print(value);
--- 6.1_function.dart
+++ 6.1_function.dart
@@ -1,14 +1,5 @@
 void main() {
   myFunction();
-  mySum(10, 9);
-
-  // ignore: unused_local_variable
-  int result = myReturn(2, 8, 4);
-  print(result);
-  print("\n");
-
-  double myDivision = doubleFunction(45, 2.5);
-  print(myDivision);
 }
 
 //function strate
@@ -19,21 +10,4 @@ void myFunction() {
   print("\n");
 }
 
-void mySum(int a, int b) {
-  print("mySum Function running");
-  print(a + b);
-}
 
-//  return function
-int myReturn(int a, int b, int c) {
-  print("Return Function running");
-  int sum = a + b + c;
-  return sum; // returning
-}
-// double return function
-
-double doubleFunction(int x, double y) {
-  print("Division value retu");
-  double divi = x / y;
-  return divi;
-}
